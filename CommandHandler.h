4125e5288931467cfe11f6056bd5e137b0b811d9
Anggoro Dwi <borni3s.burn@gmail.com>
2018-05-27 07:33:57 +0700

    Pure C

--- CommandHandler.h
+++ CommandHandler.h
@@ -1,101 +1,46 @@
 #ifndef COMMAND_HANDLER_H
 #define COMMAND_HANDLER_H
 
-//#include <Arduino.h>
-// #include <Stream.h>
-// #include <stdio.h>
-// #include <string.h>
-//#include <avr/pgmspace.h>
-
-// #undef __DEBUG__
-// #include "Debug.h"	
-
-
-// #define STD_CMD		"AN"
-
-// class CommandContainer {
-// 	public:ifndef
-// 	const char * cmd;
-// 	CommandContainer(const char * c):cmd(c) {
-
-// 	}
-// 	virtual byte exe(void);
-// 	virtual void rsp(byte);
-// };
 typedef struct ts_cmd_cntr	ts_cmd_cntr;
 typedef struct ts_cmd_hdl	ts_cmd_hdl;
-typedef unsigned char 	(*pTuc_fnc_v)	(ts_cmd_hdl * self);
-typedef void 			(*pTv_fnc_uc)	(ts_cmd_hdl * self, unsigned char);
-typedef void 			(*pTv_fnc_v)	(ts_cmd_hdl * self);
-struct ts_cmd_cntr {
-	unsigned char 	cmd[3];
-	unsigned char 	(*exe) (void);
-	void  			(*rsp) (unsigned char);
-};
-// template <unsigned int sizeCmd, unsigned int lenHeader, unsigned int sizeBuff>
-// class CommandHandler
-// {
-// private:
-// 	DBG_DECLARATION();
-// 	char Buffer[sizeBuff];
-// 	byte (*PincommingFncHanddler)(void);
-// 	void (*errorFncHandler)(byte);
-// 	CommandContainer *cc[sizeCmd];
-// 	byte poss_push,poss_pop;
-// 	void flush(void);
-// public:
-// 	CommandHandler();
-// 	void attachSys(byte(*funcIn)(void), void (*error)(byte));
-// 	void registerCmd(CommandContainer *c[]);
-// 	unsigned int maxCommand(){return sizeBuff;}
-// 	void run();
-// 	void push(char);
-// 	char pop();
-// 	void cpyChar(char * c, byte l);
-// 	byte available();
+typedef union  un_cmd_data	un_prv_data;
 
-// };
 
 struct ts_cmd_hdl {
 
-	unsigned char 	poss_push;
-	unsigned char	poss_pop;
-
-//	void 		(*attachSys) 	(ts_cmd_hdl * , pTuc_fnc_v	 , pTv_fnc_uc);
-//	void 		(*registerCmd)	(ts_cmd_hdl * , ts_cmd_cntr* , unsigned char );
-
-	pTuc_fnc_v	available;
-	pTv_fnc_v 	run;
-	pTv_fnc_uc 	push;
-	pTuc_fnc_v 	pop;
-
-	void		(*GetHeader)	(ts_cmd_hdl * self, unsigned char * c);
-	void		(*cpy2buff)		(ts_cmd_hdl * , unsigned char * , unsigned char );
+	un_prv_data 	*pPrvt;	
 	
+	ts_cmd_cntr 	*pCmdContainer;
+
+	unsigned char	(*pIncommingHdl)	(ts_cmd_hdl * self);
+	unsigned char	(*pOutgoingHdl)		(ts_cmd_hdl * self);
+	void		 	(*pErrorHdl)		(ts_cmd_hdl * self, unsigned char );
+
+	unsigned char	(*available)		(ts_cmd_hdl * self);
+	void 			(*run)				(ts_cmd_hdl * self);
+	void			(*push)				(ts_cmd_hdl * self, const unsigned char *x, unsigned int len);
+	void*			(*getParam)			(ts_cmd_hdl * self);
+	unsigned int 	(*Return) 			(ts_cmd_hdl * self, unsigned char oknok, unsigned char* p);
 };
 
-//void 			cmAttachSys		(ts_cmd_hdl * self, pTuc_fnc_v 	inc, 			pTv_fnc_uc 		err);
-//void			cmRegisterCmd	(ts_cmd_hdl * self, ts_cmd_cntr *pContainer,	unsigned char 	len);
-
-unsigned char	cmAvailable		(ts_cmd_hdl * self);
-void			cmRun			(ts_cmd_hdl * self);
-void			cmPush			(ts_cmd_hdl * self, unsigned char);
-unsigned char	cmPop			(ts_cmd_hdl * self);
-void 			cmCpy2buff		(ts_cmd_hdl * self, unsigned char * c, unsigned char len);
-void			cmGetHeader		(ts_cmd_hdl * self, unsigned char * c);
-void            cmFlush         (ts_cmd_hdl * self);
-
+struct ts_cmd_cntr {
+	const unsigned char cmd[3];
+	unsigned int 	(*exe)				(ts_cmd_hdl * self);
+};
 
 typedef struct cm_init_ts {
-	unsigned char 	(*inc) (void);
-	void  			(*err) (unsigned char);	
+	unsigned char 	(*inc)(ts_cmd_hdl * self);
+	unsigned char 	(*out)(ts_cmd_hdl * self)	;
+	void		 	(*err)(ts_cmd_hdl * self, unsigned char c)	;
 
 	ts_cmd_cntr		*container;	
+
 	unsigned char	fbyte;	
 	unsigned char 	len;
+	unsigned char	sizeBuf;
 }cm_init_ts;
 
-void CM_SYS_INIT( ts_cmd_hdl * cm , cm_init_ts * init );
+ts_cmd_hdl 	cmNew_Command(cm_init_ts * init );
 
 //#include "cpp/CommandHandler.cpp"
 #endif //COMMAND_HANDLLER_H
--- README.md
+++ README.md
@@ -1,92 +1,12 @@
-CommandHandler
-==============
-Handle Serial Command for AVR.
-Depedencie : https://github.com/sudar/MemoryFree
+# CommandHandler API
 
-Format
-======
-| < STD_CMD >+ | < COMMAND > | =< PARAMETER > |
-|:--------:|:-------:|:----------:|
-|    M     |    M    |      O     |
+CommandHanddler is a API that handle command management. User can create command with/without parameter, doing something for it and then send the respond.
 
-M:Mandatory
-O:Optional
+## Format 
 
-Define 
-======
-### #define STD_CMD  "ANU"
+Format is simple. First 5 Byte contain the header
 
-This define used for header command for identity. like AT command with 'AT' Header for send data behind.
+| **First_Byte** | **CMD_0** | **CMD_1** | **CMD_2** | **Lengt_Paramater** | *Data_Parameter* |
+| -------------- | --------- | --------- | --------- | ------------------- | ---------------- |
+| 1 Byte         | 1 Byte    | 1 Byte    | 1 Byte    | 1 Byte              | n Byte           |
 
-##### Example with "ANU"
-Serial : ANU+CMD1
-
-### #define CHAR_CMD		5
-
-This define for limitation of C char(end char with '\0') < COMMAND > . 
-
-##### Example with 5
-Serial : ANU+CMD1   //OK
-Serial : ANU+COMD1  //ERROR becouse no place of'\0'
-Serial : ANU+COMAND //ERROR
-
-### #define CMD_BUFFER		127
-
-This define for length buffer.
-
-Function
-========
-``` C++
-CommandHandler(byte(*funcIn)(void), void (*error)(void),const TSCmdTbl *cmdTbl,byte len);
-```
-Constructor.
-
-| Parameter | Description |
-| --- | --- |
-| byte(*funcIn)(void) | pointer to function for handle incomming data. |
-| void (*error)(void) | pointer to function for handle error format for incomming data. |
-| const TSCmdTbl *cmdTbl | pointer to TSCmdTbl that have list of command and function to run. |
-| byte len | length of command in TSCmdTbl that you make. |
-
-____
-
-``` C++
-void run();
-```
-
-Run All Command.
-
-____
-
-``` C++
-byte available();
-```
-To know how  much char of < PARAMETER > that save in buffer
-
-| Retrun | Description |
-| --- | --- |
-| byte x | length of char in buffer for < PARAMETER >. |
-
-____
-
-``` C++
-void push(char x);
-```
-save a char to buffer.
-
-| Parameter | Description |
-| --- | --- |
-| char x | char that save in buffer. |
-
-____
-
-``` C++
-char pop();
-```
-pop of char in < PARAMETER >.
-
-| Retrun | Description |
-| --- | --- |
-| char x | pop the char for < PARAMETER > in buffer. |
-
-____
